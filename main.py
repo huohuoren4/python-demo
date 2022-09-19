84c1f5f1714d28f12b28626e68a51ffd002b255e
tom01 <674860357@qq.com>
2022-09-19 23:18:12 +0800

    仓库从Gitee转移到Github

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+import pytest
+from selenium import webdriver
+
+if __name__ == '__main__':
+    # pytest.main()
+    # cmd = r"allure generate ./allure_reports/tmp  -o ./allure_reports/html --clean"
+    # os.system(cmd)
+
+    driver= webdriver.Chrome()
+    driver.find_element().text
