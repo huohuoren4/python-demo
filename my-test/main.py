e1739e2841d3dde8885d253eeff6ebe0482037a6
tom01 <674860357@qq.com>
2022-11-07 21:23:03 +0800

    2022-11-07: 测试gitee的前置同步

--- my-test/main.py
+++ my-test/main.py
@@ -1,21 +1,9 @@
+import random
 from time import sleep
 
-
-class MyClass:
-
-    def __init__(self, name, password) -> None:
-        self.name= name
-        self.password= password
-
-    def get(self):
-        print(self.name, self.password)
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 if __name__ == '__main__':
-    arr=[]
-    for _ in range(5000000):
-        obj= MyClass("123", 30)
-        arr.append(obj)
-
-    print("\033[32mSuccess: 循环结束 (^_^)\033[0m", len(arr))
-    sleep(100)
+    # psuti
+    pass
